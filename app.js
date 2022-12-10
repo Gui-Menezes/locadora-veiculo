a1276466a9c25b5893842b37a14b38c4d14fd443
Guilherme <guibenmen@gmail.com>
2022-12-10 14:43:33 -0300

    Exclusão de códigos duplicados no app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const { main } = require('./main')
 const { main_express } = require('./main_express')
-const clienteNegocio = require('./negocio/cliente_negocio')
-const locacaoNegocio = require('./negocio/locacao_negocio')
-const veiculoNegocio = require('./negocio/veiculo_negocio')
-const express = require('express')
-const app = express()
-const port = 3000
 
 //main()
 
