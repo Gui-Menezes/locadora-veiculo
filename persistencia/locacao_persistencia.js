ec2ea85fe3039f226cf8acba9e5f2fece9abaa01
Guilherme <guibenmen@gmail.com>
2022-12-10 14:39:08 -0300

    Inclusão de INNER JOIN para mostrar nome do cliente e modelo do veículo na função listar da locação

--- persistencia/locacao_persistencia.js
+++ persistencia/locacao_persistencia.js
@@ -5,7 +5,12 @@ async function listar(){
     const cliente = new Client(conexao)
     await cliente.connect()
 
-    const res = await cliente.query('SELECT * FROM locacao')
+    const res = await cliente.query(
+        `SELECT locacao.cod_loc, cliente.cod_cli, cliente.nome, veiculo.cod_auto, veiculo.modelo, veiculo.chassi
+        FROM locacao
+        INNER JOIN cliente on (cliente.cod_cli = locacao.cod_cli)
+        INNER JOIN veiculo on (veiculo.cod_auto = locacao.cod_auto)`
+        )
     await cliente.end()
 
     return res.rows;
